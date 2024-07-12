8346015c69567c7e0ed00c3906e627d762097b5c
samaila-bashir <developer.chatto@gmail.com>
2024-07-12 02:37:44 +0100

    created charts for total section on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,11 @@
+import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
 
 const Home = () => {
@@ -7,11 +14,19 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Box 2</div>
-      <div className="box box3">Box 3</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
       <div className="box box4">Box 4</div>
-      <div className="box box5">Box 5</div>
-      <div className="box box6">Box 6</div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
       <div className="box box7">Box 7</div>
       <div className="box box8">Box 8</div>
       <div className="box box9">Box 9</div>
