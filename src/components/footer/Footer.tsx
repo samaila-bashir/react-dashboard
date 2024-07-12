c2e95febd352e6af8db756baec88e2b65573d5ec
samaila-bashir <developer.chatto@gmail.com>
2024-07-12 01:10:42 +0100

    created left menu area

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>React Dashboard</span>
+      <span>&copy; React Admin Dashboard</span>
+    </div>
+  );
 };
 export default Footer;
